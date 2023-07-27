cf8a4e9de4530b176f0bacce2196f19c59d0f76a
unknown <yuvrajparashar710@gmail.com>
2023-07-27 23:50:11 +0530

    hover effect done

--- src/components/DisplaySingle.jsx
+++ src/components/DisplaySingle.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import '../App.css';
-import star1 from '../images/star1.ico';
-import star2 from '../images/star2.ico';
-
+import "../App.css";
+import star1 from "../images/star1.ico";
+import star2 from "../images/star2.ico";
+import {useState} from 'react';
 function DisplaySingle(props) {
   // console.log("props sddsss", props.single_product);
+
+  const [isHover, setIsHover] = useState(false)
   const handleIconChange = () => {
     let da = props.dataArray.filter((item) => {
       if (item.id === props.single_product.id) {
@@ -14,25 +16,40 @@ function DisplaySingle(props) {
     });
     props.handleDataArray(da);
   };
+
   return (
     <div className="single-product">
-      <div className="icon" onClick={handleIconChange}>
-      {
-        props.single_product.isFav?
-        <img src={star1} alt="" style={{width:'20px'}}/>
-        :<img src={star2} alt="" style={{width:'20px'}}/>
-      }
+      {/* <img className="product-image" src={props.single_product.img} alt="" /> */}
+      <div
+        style={{
+          backgroundImage: `URL(${props.single_product.img})`,
+          height: "300px",
+        }}
+        className="background-img"
+        onMouseEnter={(e) => {setIsHover(true)}}
+        onMouseLeave={(e) => {setIsHover(false)}}
+      >
+        <div className="icon" onClick={handleIconChange}>
+          {props.single_product.isFav ? (
+            <img src={star1} alt="" style={{ width: "25px" }} />
+          ) : (
+            <img src={star2} alt="" style={{ width: "25px" }} />
+          )}
+        </div>
+        <div className={isHover?'product-hover-txt':'display-none'}>
+          <p>
+            show product
+            </p>
+        </div>
       </div>
-      <img className="product-image" src={props.single_product.img} alt="" />
-
       <p>{props.single_product.id}</p>
-      <h1>{props.single_product.name}</h1>
-      <p>{props.single_product.department}</p>
-      <p>{props.single_product.price}</p>
-      <p>{props.single_product.product}</p>
-      <p>{props.single_product.productAdjective}</p>
-      <p>{props.single_product.desc}</p>
-      <p>{props.single_product.productMaterial}</p>
+      <p>{props.single_product.name}</p>
+      {/* <p>{props.single_product.department}</p> */}
+      {/* <p>{props.single_product.price}</p> */}
+      {/* <p>{props.single_product.product}</p> */}
+      {/* <p>{props.single_product.productAdjective}</p> */}
+      {/* <p>{props.single_product.desc}</p> */}
+      {/* <p>{props.single_product.productMaterial}</p> */}
     </div>
   );
 }
